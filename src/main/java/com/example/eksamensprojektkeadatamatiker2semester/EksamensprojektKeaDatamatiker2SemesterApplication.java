cb6a1ebc30c46997e42ff33f14bc46e4c725adbe
Mojens <moha135h@stud.kea.dk>
2022-05-09 16:12:03 +0200

    First commit

---
+++ src/main/java/com/example/eksamensprojektkeadatamatiker2semester/EksamensprojektKeaDatamatiker2SemesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamensprojektkeadatamatiker2semester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamensprojektKeaDatamatiker2SemesterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EksamensprojektKeaDatamatiker2SemesterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/eksamensprojektkeadatamatiker2semester/EksamensprojektKeaDatamatiker2SemesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksamensprojektkeadatamatiker2semester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamensprojektKeaDatamatiker2SemesterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
