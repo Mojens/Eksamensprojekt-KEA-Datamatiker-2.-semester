655a85098df050559698d01c9c54b92f8b355ede
Mojens <moha135h@stud.kea.dk>
2022-05-10 14:40:23 +0200

    ConnectionManager.java, added package config and added new javaclass for picture upload

--- src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Utility/ConnectionManager.java
+++ src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Utility/ConnectionManager.java
@@ -1,4 +1,27 @@
 package com.example.eksamensprojektkeadatamatiker2semester.Utility;
 
-public class ConnectionManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public abstract class ConnectionManager {
+
+  private static Connection connection;
+
+  public static Connection getConnection() {
+    String URL = System.getenv("url");
+    String UID = System.getenv("user");
+    String PWD = System.getenv("pwd");
+
+    if (connection == null) {
+      try {
+        connection = DriverManager.getConnection(URL, UID, PWD);
+        System.out.println("Der er forbundet til din Database");
+      } catch (Exception e) {
+        System.out.println("Der blev ikke forbundet til din Database: ");
+        e.printStackTrace();
+      }
+    }
+    return connection;
+  }
+
 }
