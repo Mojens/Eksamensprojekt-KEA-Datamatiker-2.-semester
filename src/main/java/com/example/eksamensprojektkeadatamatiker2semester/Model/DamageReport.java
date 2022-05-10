ca193347cc08e9f18834dfb0cdbbdb1d24f4ccee
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-05-10 15:58:10 +0200

    Boilerplade code DamageReport

--- src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Model/DamageReport.java
+++ src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Model/DamageReport.java
@@ -1,4 +1,42 @@
 package com.example.eksamensprojektkeadatamatiker2semester.Model;
 
+import java.util.ArrayList;
+
 public class DamageReport {
+    private ArrayList<SpecificDamage> specificDamage;
+    private User userID;
+    private Lease leaseID;
+
+    public DamageReport(ArrayList<SpecificDamage> specificDamage, User userID, Lease leaseID) {
+        this.specificDamage = specificDamage;
+        this.userID = userID;
+        this.leaseID = leaseID;
+    }
+
+    public DamageReport() {
+    }
+
+    public ArrayList<SpecificDamage> getSpecificDamage() {
+        return specificDamage;
+    }
+
+    public void setSpecificDamage(ArrayList<SpecificDamage> specificDamage) {
+        this.specificDamage = specificDamage;
+    }
+
+    public User getUserID() {
+        return userID;
+    }
+
+    public void setUserID(User userID) {
+        this.userID = userID;
+    }
+
+    public Lease getLeaseID() {
+        return leaseID;
+    }
+
+    public void setLeaseID(Lease leaseID) {
+        this.leaseID = leaseID;
+    }
 }
