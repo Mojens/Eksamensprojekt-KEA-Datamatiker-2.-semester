94d2363f6d5bd97a33774ed0e2c3d255a0b48725
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-05-10 16:09:25 +0200

    Boilerplate code ShowKPI

--- src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Model/ShowKPI.java
+++ src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Model/ShowKPI.java
@@ -1,4 +1,30 @@
 package com.example.eksamensprojektkeadatamatiker2semester.Model;
 
 public class ShowKPI {
+    private Car car;
+    private Lease lease;
+
+    public ShowKPI(Car car, Lease lease) {
+        this.car = car;
+        this.lease = lease;
+    }
+
+    public ShowKPI() {
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public Lease getLease() {
+        return lease;
+    }
+
+    public void setLease(Lease lease) {
+        this.lease = lease;
+    }
 }
