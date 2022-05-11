7a4427a93226d057d91c389ea9e08a0a57328ced
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-05-11 09:51:25 +0200

    connection

--- src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Repository/ShowKPIRepository.java
+++ src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Repository/ShowKPIRepository.java
@@ -1,4 +1,13 @@
 package com.example.eksamensprojektkeadatamatiker2semester.Repository;
 
+import com.example.eksamensprojektkeadatamatiker2semester.Utility.ConnectionManager;
+
+import java.sql.Connection;
+
 public class ShowKPIRepository {
+    Connection connection;
+
+    public ShowKPIRepository(){
+        connection = ConnectionManager.getConnection();
+    }
 }
