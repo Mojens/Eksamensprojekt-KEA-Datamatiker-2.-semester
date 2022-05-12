a33f556921c763a5918588ae6829afd90053f267
Mojens <moha135h@stud.kea.dk>
2022-05-13 00:41:19 +0200

    ShowKPI virker nu bare uden design

--- src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Controller/ShowKPIController.java
+++ src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Controller/ShowKPIController.java
@@ -1,4 +1,39 @@
 package com.example.eksamensprojektkeadatamatiker2semester.Controller;
 
+import com.example.eksamensprojektkeadatamatiker2semester.Model.Car;
+import com.example.eksamensprojektkeadatamatiker2semester.Repository.ShowKPIRepository;
+import com.example.eksamensprojektkeadatamatiker2semester.Service.ShowKPIService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
 public class ShowKPIController {
+  ShowKPIRepository showKPIRepository;
+  ShowKPIService showKPIService;
+  public ShowKPIController(ShowKPIRepository showKPIRepository,
+                           ShowKPIService showKPIService){
+    this.showKPIRepository = showKPIRepository;
+    this.showKPIService = showKPIService;
+  }
+
+  @GetMapping("/showKPI")
+  public String showKPI(Model model,
+                        HttpSession httpSession){
+    List<Car> leasedCars = showKPIRepository.addLeasedCarsToList();
+    int amountOfLeasedCars = showKPIService.howManyisLeased(leasedCars);
+    double totalPriceOfLeasedCars = showKPIService.totalPriceLeasedCar(leasedCars);
+    System.out.println(amountOfLeasedCars);
+    System.out.println(totalPriceOfLeasedCars);
+    model.addAttribute("listOfLeasedCars",leasedCars);
+    model.addAttribute("totalPriceOfLeasedCars",totalPriceOfLeasedCars);
+    model.addAttribute("amountOfLeasedCars",amountOfLeasedCars);
+    return "/showKPI";
+  }
+
+
+
 }
---
+++ src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Service/ShowKPIService.java
@@ -0,0 +1,31 @@
+package com.example.eksamensprojektkeadatamatiker2semester.Service;
+
+import com.example.eksamensprojektkeadatamatiker2semester.Model.Car;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ShowKPIService {
+
+  //Tjekker hvormange car objecter der er leased ud fra en liste
+  public int howManyisLeased(List<Car> leasedCars){
+    int amountOfLeased = 0;
+    for (Car c:
+         leasedCars) {
+      amountOfLeased = amountOfLeased + 1;
+
+    }
+    return amountOfLeased;
+  }
+
+  public double totalPriceLeasedCar(List<Car> leasedCars){
+    double totalPrice = 0;
+    for (Car c:
+         leasedCars) {
+    totalPrice = totalPrice + c.getPrice();
+    }
+    return totalPrice;
+  }
+
+}
