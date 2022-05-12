cc1ee4f548a5afea059b6a689ce04b4a89fd1f83
Mojens <moha135h@stud.kea.dk>
2022-05-12 12:43:44 +0200

    fix i home controller

--- src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Controller/UserController.java
+++ src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Controller/UserController.java
@@ -38,7 +38,7 @@ public class UserController {
       return userService.checkTypeByUser(String.valueOf(loggedUser.getType()));
     } else
       model.addAttribute("Failed Login", "Failed login");
-    return "/redirect:/login";
+    return "redirect:/login";
   }
 
 
