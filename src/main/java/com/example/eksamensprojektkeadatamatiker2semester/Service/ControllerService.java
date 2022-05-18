9544b7b25fa3e9bc8b3ee1da9f2bb0e1559a2054
Mojens <moha135h@stud.kea.dk>
2022-05-18 11:22:00 +0200

    Fixes redirection og låst de forskellige bruger rettigheder

--- src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Service/ControllerService.java
+++ src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Service/ControllerService.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 @Service
 public class ControllerService {
+
 // Type 1
   public String opretLejeAftaler(HttpSession httpSession) {
     String currentPage = null;
