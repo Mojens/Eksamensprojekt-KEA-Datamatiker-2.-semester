5c33fcad3f2cb22466a9c1d941ab753e072bd11d
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-06-20 19:12:08 +0200

    Kommentar på SpecificDamageController og FileUploadUtil

--- src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Utility/FileUploadUtil.java
+++ src/main/java/com/example/eksamensprojektkeadatamatiker2semester/Utility/FileUploadUtil.java
@@ -10,6 +10,16 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 /* Lavet Af Malthe */
 public class FileUploadUtil {
+
+  // Metoden har 3 parameter, en uploadDir, et fileName og en multipartFile
+  // uploadDir er selve mappen hvor filen bliver gemt
+  // fileName er navnet på filen som bliver gemt i databasen som en String.
+  // MultipartFile er en midlertidigt representation af en uploadet fil som er blevet modtaget i en multipart request.
+  // Selve filnavnet bliver midlertidigt gemt i filsystemet via MultipartFile interfacen
+  // og derefter permanent gemt i databasen i metoden addSpecificDamage i SpecificDamageController klassen.
+
+  // Selve metoden er kun ansvarlig for at oprette mappen, hvis den ikke eksisterer,
+  // og gemme den uploadede fil fra MultipartFile-objektet til en fil i filsystemet.
   public static void saveFile(String uploadDir, String fileName,
                               MultipartFile multipartFile) throws IOException {
     Path uploadPath = Paths.get(uploadDir);
